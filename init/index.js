4f95f8230541f982e1323053dc926defe26b488d
sahithinandikula <sahithinandikula05@gmail.com>
2024-12-07 16:38:07 +0530

    Changes Added

--- init/index.js
+++ init/index.js
@@ -17,6 +17,10 @@ async function main(){
 
 const initDB=async() =>{
      await Listing.deleteMany({});
+     initData.data=initData.data.map((obj)=>({
+        ...obj,
+        owner: "67530d09c54ddb831a33451f",
+     }));
      await Listing.insertMany(initData.data);
      console.log("data was initialied");
 };
--- routes/listing.js
+++ routes/listing.js
@@ -4,6 +4,7 @@ const wrapAsync=require("../utils/wrapAsync.js");
 const ExpressError=require("../utils/ExpressError.js");
 const {listingSchema}=require("../schema.js");
 const Listing=require("../models/listing.js");
+const {isLoggedIn}=require("../middleware.js");
 
 
 
@@ -25,7 +26,7 @@ router.get("/",wrapAsync(async (req,res)=>{
    }));
    
    //Create new route
-   router.get("/new",(req,res)=>{
+   router.get("/new",isLoggedIn,(req,res)=>{
        res.render("listings/new.ejs");
    });
    
@@ -35,13 +36,13 @@ router.get("/",wrapAsync(async (req,res)=>{
         const listing =await Listing.findById(id).populate("reviews");
         if(!listing){
             req.flash("error","Listing you requested for does not exist");
-            return res.redirect("/listings");
+            return   res.redirect("/listings");
         }
         res.render("listings/show.ejs",{listing});
    }));
 
    //create route
-router.post("/",validateListing,wrapAsync(async(req,res,next)=>{
+router.post("/",isLoggedIn,validateListing,wrapAsync(async(req,res,next)=>{
     const newListing=new Listing(req.body.listing);
     await newListing.save();
     req.flash("success","listing created successfully");
@@ -50,7 +51,7 @@ router.post("/",validateListing,wrapAsync(async(req,res,next)=>{
 }));
 
 //edit route
-router.get("/:id/edit",wrapAsync(async (req,res)=>{
+router.get("/:id/edit",isLoggedIn,wrapAsync(async (req,res)=>{
  let {id}=req.params;
  const listing=await Listing.findById(id);
  if(!listing){
@@ -61,7 +62,7 @@ router.get("/:id/edit",wrapAsync(async (req,res)=>{
 }));
 
 //update route
-router.put("/:id",validateListing,wrapAsync(async (req,res)=>{
+router.put("/:id",isLoggedIn,validateListing,wrapAsync(async (req,res)=>{
    let {id}=req.params;
     await Listing.findByIdAndUpdate(id,{...req.body.listing});
     req.flash("success","listing Updated");
@@ -69,7 +70,7 @@ router.put("/:id",validateListing,wrapAsync(async (req,res)=>{
 }));
 
 //delete route
-router.delete("/:id",wrapAsync(async(req,res)=>{
+router.delete("/:id",isLoggedIn,wrapAsync(async(req,res)=>{
 let {id}=req.params;
 let deletedListing=await Listing.findByIdAndDelete(id);
  console.log(deletedListing);
